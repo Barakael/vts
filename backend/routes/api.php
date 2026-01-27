5f3a06e0e6416ff4208e5dc62648928159d1e8c7
barakael <barakaellucas2019@gmail.com>
2026-01-27 22:59:09 +0300

    Fix typo in AuthApiController reference and update user route to use controller method

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,7 @@
-<?php
+<?php 
+use App\Http\Controllers\AuthApiController;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthApiController::class, 'register']);
+Route::post('/login', [AuthApiController::class, 'login']);
+Route::post('logout', [AuthApiController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('user', [AuthApiContrioller::class, 'user'])->middleware('auth:sanctum');
